3d674386eb282a0d5e08b2d89670569611135cb5
William Slatton <williamslatton11@gmail.com>
2016-09-27 16:47:11 -0500

    added ftc-nav module and a basic container for position data

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NavTest.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import org.lasarobotics.ftcnav.*;
+
+public class NavTest extends OpMode {
+    Robot robot;
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
