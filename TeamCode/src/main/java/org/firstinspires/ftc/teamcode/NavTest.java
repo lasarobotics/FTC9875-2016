c36552a17a987af065344fb4f319504c1d480d45
William Slatton <williamslatton11@gmail.com>
2016-10-26 16:28:49 -0500

    added ftc-library

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NavTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NavTest.java
@@ -1,8 +1,13 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
 import org.lasarobotics.ftcnav.*;
 
+@TeleOp(name = "nav", group = "test")
+@Disabled
 public class NavTest extends OpMode {
     Robot robot;
 
