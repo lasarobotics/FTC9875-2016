ed20665856c3b98f24fe8b56a14737f2dff89d2d
William Slatton <williamslatton11@gmail.com>
2016-11-09 11:16:25 -0600

    added servo toggle for beacon, need to test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTestLib.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTestLib.java
@@ -2,11 +2,15 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-@TeleOp(name = "mechanum-lib", group = "test")
+@TeleOp(name = "mecanum-lib", group = "test")
 public class MecanumTestLib extends OpMode {
     DcMotor left_back, left_front, right_back, right_front;
+    CRServo arm;
+    boolean arm_moving = false;
+    double time_last_toggle = 0;
     private float tol = 0.05f;
 
     public void init() {
@@ -14,10 +18,18 @@ public class MecanumTestLib extends OpMode {
         left_front = hardwareMap.dcMotor.get("left_front");
         right_back = hardwareMap.dcMotor.get("right_back");
         right_front = hardwareMap.dcMotor.get("right_front");
+        arm = hardwareMap.crservo.get("servo1");
     }
 
     public void loop() {
         Mecanum.arcade(damp(tol, gamepad1.left_stick_y), damp(tol, gamepad1.left_stick_x), damp(tol, gamepad1.right_stick_x), left_front, right_front, left_back, right_back);
+        if(gamepad1.a) {
+            if(getRuntime() - time_last_toggle > 0.25) {
+                time_last_toggle = getRuntime();
+                arm_moving = !arm_moving;
+            }
+        }
+        arm.setPower(arm_moving ? 0.5 : 0);
     }
 
     public void stop() {
@@ -25,6 +37,7 @@ public class MecanumTestLib extends OpMode {
         left_front.setPower(0);
         right_back.setPower(0);
         right_front.setPower(0);
+        arm.setPower(0);
     }
 
     private float damp(float tol, float val) {
