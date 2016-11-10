95a4be7933a5fdbe53a901a685297b905ef7f6f7
Russell Coleman <russellsayshi@gmail.com>
2016-11-10 17:36:25 -0600

    Added simple auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SimpleAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SimpleAuto.java
@@ -6,22 +6,33 @@ import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 @TeleOp(name = "simpledrive", group = "test")
-public class DriveForward extends OpMode {
+public class SimpleAuto extends OpMode {
     DcMotor left_back, left_front, right_back, right_front, intake, shooter;
+    boolean timeSet = false;
+    long time;
 
     public void init() {
         left_back = hardwareMap.dcMotor.get("left_back");
         left_front = hardwareMap.dcMotor.get("left_front");
         right_back = hardwareMap.dcMotor.get("right_back");
         right_front = hardwareMap.dcMotor.get("right_front");
+        timeSet = false;
     }
 
     public void loop() {
-        float power = damp(0.05f, gamepad1.left_stick_y);
-        left_back.setPower(power);
-        right_back.setPower(power);
-        left_front.setPower(power);
-        right_front.setPower(power);
+        if(!timeSet) {
+            time = System.currentTimeMillis();
+            timeSet = true;
+        }
+        if(System.currentTimeMillis() - time < 1000) {
+            Mecanum.arcade(0, 0.5f, 0, left_front, right_front, left_back, right_back);
+        } else {
+            int power = 0;
+            left_back.setPower(power);
+            right_back.setPower(power);
+            left_front.setPower(power);
+            right_front.setPower(power);
+        }
     }
 
     public void stop() {
