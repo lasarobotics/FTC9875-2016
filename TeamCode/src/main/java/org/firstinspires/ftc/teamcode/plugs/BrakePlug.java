c7b57100aa32007c2dc492eac3f9e8230bc852eb
Russell Coleman <russellsayshi@gmail.com>
2017-02-27 17:13:29 -0600

    update to fix braking

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/plugs/BrakePlug.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/plugs/BrakePlug.java
@@ -7,6 +7,7 @@ public class BrakePlug implements Plug {
     public void init(Robot robot) {
 
     }
+    private boolean just_broke = false;
     public void loop(Robot robot) {
         if(robot.c1.right_bumper()) {
             robot.left_back.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
@@ -18,6 +19,13 @@ public class BrakePlug implements Plug {
             robot.left_front.setPower(0);
             robot.right_back.setPower(0);
             robot.right_front.setPower(0);
+            just_broke = true;
+        } else if(just_broke) {
+            robot.left_back.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+            robot.left_front.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+            robot.right_back.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+            robot.right_front.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+            just_broke = false;
         }
     }
     public void stop(Robot robot) {
